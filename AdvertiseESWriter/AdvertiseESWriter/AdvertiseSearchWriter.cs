ecb250d8506bf92b2cd25569dfd1f1f2e4184d93
Ipsa <71475293+Ipsa84@users.noreply.github.com>
2020-10-11 22:29:57 -0700

    Initial Add

---
+++ AdvertiseESWriter/AdvertiseESWriter/AdvertiseSearchWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+using Newtonsoft.Json;
+using Nest;
+using AdvertiseApi.Models;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace AdvertiseESWriter
+{
+    public class AdvertiseSearchWriter
+    {
+        public AdvertiseSearchWriter() : this(ElasticSearchWriter.GetInstance(ConfigHelper.Instance))
+        {
+
+        }
+
+        private readonly IElasticClient _client;
+        public AdvertiseSearchWriter(IElasticClient client)
+        {
+            _client = client;
+        }
+        public async Task Function(SNSEvent snsEvent, ILambdaContext context)
+        {
+
+            foreach (var record in snsEvent.Records)
+            {
+                context.Logger.LogLine(record.Sns.Message);
+
+                var message = JsonConvert.DeserializeObject<ConfirmedAdvertisementMsg>(record.Sns.Message);
+                var advertDocument = Mapper.Map(message);
+                await _client.IndexDocumentAsync(advertDocument);
+
+            }
+        }
+    }
+}
---
+++ AdvertiseESWriter/AdvertiseESWriter/Mapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AdvertiseApi.Models;
+namespace AdvertiseESWriter
+{
+    public static class Mapper
+    {
+        public static AdvertiseType Map(ConfirmedAdvertisementMsg message)
+        {
+            var doc = new AdvertiseType
+            {
+                Id = message.Id,
+                Title = message.Title,
+                CreationDateTime = DateTime.UtcNow
+            };
+            return doc;
+        }
+    }
+}
